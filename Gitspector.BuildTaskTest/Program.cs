76d5bfcdc25499ab223c5d2dde33c19f58848ddc
Szabadkai Mark <mqrelly@gmail.com>
2012-05-18 17:37:49 +0200

    Add BuildTaskTest proj with working example.

---
+++ Gitspector.BuildTaskTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gitspector.BuildTaskTest
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            string head_commit;
+            var this_assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var stream = this_assembly.GetManifestResourceStream("Gitspector.BuildTaskTest.git-head-sha1.txt");
+
+            using( var reader = new System.IO.StreamReader(stream) )
+                head_commit = reader.ReadToEnd().Trim();
+
+            Console.WriteLine("My git HEAD SHA-1 was {0} when I was built.", head_commit);
+        }
+    }
+}
---
+++ Gitspector/Tasks/InspectGitRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Build.Framework;
+
+namespace Gitspector.Tasks
+{
+    public class InspectGitRepo : Microsoft.Build.Utilities.Task
+    {
+        private Repository repo;
+
+        [Required]
+        public string StartLookingInDir { get; set; }
+
+        [Output]
+        public string HeadCommit
+        {
+            get { return this.repo.HeadCommit; }
+        }
+
+        public override bool Execute()
+        {
+            this.repo = new Repository(this.StartLookingInDir);
+            return true;
+        }
+    }
+}
